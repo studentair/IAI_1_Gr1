e52d44f8c6080642236acb48a9a1c98fdbc37c17
adam kowalski <adamakowa@gmail.com>
2018-11-26 17:17:44 +0100

    Kol1

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('.button');
+
+button.addEventListener('click', function () {
+    let box = document.querySelector('.box')
+        background-color = 'red';
+}
+
